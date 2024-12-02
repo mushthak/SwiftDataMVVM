cbd44ec91afdd13467f1793ae76387b6bf776fc6
Mushthak Ebrahim <mushthakebrahim@gmail.com>
2024-12-02 16:26:57 +0530

    Initial Commit

---
+++ SwiftDataMVVMTests/SwiftDataMVVMTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataMVVMTests.swift
+//  SwiftDataMVVMTests
+//
+//  Created by Mushthak Ebrahim on 02/12/24.
+//
+
+import Testing
+@testable import SwiftDataMVVM
+
+struct SwiftDataMVVMTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
